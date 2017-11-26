55e1d5a2cf161334b23d85886c314ee4b41ad661
teenmaz <teenmaz@gmail.com>
2017-11-26 19:25:32 +0700

    add struct response

--- modules/auth/model.go
+++ modules/auth/model.go
@@ -1,1 +1,26 @@
 package auth
+
+import "bitbucket.org/2tgroup/ciwp-api-users/modules/users"
+
+type authUser struct {
+	ID       string      `json:"_id"`
+	Email    string      `json:"email"`
+	Name     string      `json:"name"`
+	UserType string      `json:"user_type"`
+	Status   int         `json:"status"`
+	Info     interface{} `json:"info"`
+}
+
+type AuthResponse struct {
+	Token    string   `json:"token"`
+	UserInfo authUser `json:"user"`
+}
+
+//AuthSetResponse format data response
+func (au *AuthResponse) AuthSetResponse(user users.UserBase) {
+	au.UserInfo.ID = user.ID.Hex()
+	au.UserInfo.Name = user.Name
+	au.UserInfo.Email = user.Email
+	au.UserInfo.UserType = user.UserType
+	au.UserInfo.Info = user.UserInfo
+}
