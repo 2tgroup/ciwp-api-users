cb34b30965d8f765356c2f2ad4d6f0e4954e5f5a
teenmaz <teenmaz@gmail.com>
2017-11-27 19:27:28 +0700

    config style error

--- types/response.go
+++ types/response.go
@@ -24,3 +24,24 @@ func PayloadResponseOk(data, meta interface{}) clientResponse {
 		Meta:    meta,
 	}
 }
+
+const (
+	//DataInvaild mess wrong struct data
+	DataInvaild = "data_invaild"
+	// ReqInvaild wrong struct data after bind
+	ReqInvaild = "request_invaild"
+	// ActionInvaild can not do some action
+	ActionInvaild = "action_invaild"
+	//NotValidate can not validate
+	NotValidate = "not_validated"
+	//ActionNotfound is not found somethings
+	ActionNotfound = "not_found"
+	//DataExist is exists something
+	DataExist = "data_exists"
+	//DataNotFound is exists something
+	DataNotFound = "data_not_found"
+	//ActionSuceess is done some action
+	ActionSuceess = "action_success"
+	//ActionError is action make error
+	ActionError = "action_error"
+)
